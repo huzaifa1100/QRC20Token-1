9d6abfdf0ec77e1fa8cd3b844a50c7637dcc4ebf
huzaifa1100 <huzaifa1100@gmail.com>
2021-06-23 23:27:21 +0500

    safemath update

--- SafeMath.sol
+++ SafeMath.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.24;
+pragma solidity  0.4.24;
 
 /* taking ideas from FirstBlood token */
 contract SafeMath {
@@ -8,17 +8,20 @@ contract SafeMath {
 
     function safeAdd(uint256 _x, uint256 _y) pure internal returns (uint256) {
         uint256 z = _x + _y;
+        //assert replaced with require *
         require(z >= _x);
         return z;
     }
 
     function safeSub(uint256 _x, uint256 _y) pure internal returns (uint256) {
+        //assert replaced with require *
         require(_x >= _y);
         return _x - _y;
     }
 
     function safeMul(uint256 _x, uint256 _y) pure internal returns (uint256) {
         uint256 z = _x * _y;
+        //assert replaced with require *
         require(_x == 0 || z / _x == _y);
         return z;
     }
